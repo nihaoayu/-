a7ade4d77019f3b2417533fd4b04a166f590b660
啊宇 <2961062796@qq.com>
2022-02-08 16:30:24 +0800

    路由守卫及进度条

--- src/permission.js
+++ src/permission.js
@@ -1,25 +1,36 @@
-// import router from './router'
-// import store from './store'
+import router from './router'
+import store from './store'
 // import { Message } from 'element-ui'
-// import NProgress from 'nprogress' // progress bar
-// import 'nprogress/nprogress.css' // progress bar style
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
 // import { getToken } from '@/utils/auth' // get token from cookie
 // import getPageTitle from '@/utils/get-page-title'
 
 // NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-// const whiteList = ['/login'] // no redirect whitelist
+const whiteList = ['/login', '/404'] // no redirect whitelist
+
+router.beforeEach(async (to, from, next) => {
+  NProgress.start()
+  if (store.getters.token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+  NProgress.done()
+})
 
 // router.beforeEach(async(to, from, next) => {
 //   // start progress bar
 //   NProgress.start()
-
-//   // set page title
-//   document.title = getPageTitle(to.meta.title)
-
-//   // determine whether the user has logged in
-//   const hasToken = getToken()
-
 //   if (hasToken) {
 //     if (to.path === '/login') {
 //       // if is logged in, redirect to the home page
